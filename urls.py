16d0e3ad1f15dbebe19e359e55c3b66beeeb8ea8
Tyler Ball <tyler@tylerball.net>
2011-03-31 16:27:42 -0400

    added tags and shorturls to urls

--- urls.py
+++ urls.py
@@ -21,7 +21,8 @@ urlpatterns = patterns('',
 	(r'^$', 'stories.views.index_front'),
 	(r'^blogs/', include('blog.urls')),
 	(r'^staff/', include('staff.urls')),
-	#(r'^rss/(?P<url>.*)/', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+	(r'^search/', include('haystack.urls')),
+	(r'^rss/(?P<url>.*)/', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 	
 	# Uncomment the admin/doc line below to enable admin documentation:
 	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -29,5 +30,18 @@ urlpatterns = patterns('',
 	# Uncomment the next line to enable the admin:
 	(r'^admin/', include(admin.site.urls)),
 	
+	(r'^tags/$', 'stories.views.tags'),
+	(r'^tag/(?P<tag>[a-zA-Z0-9_.-]+)/$','stories.views.with_tag'),
+	(r'^tag/(?P<tag>[a-zA-Z0-9_.-]+)/page/(?P<id>[-\w]+)/$', 'stories.views.with_tag' ),
+	
+	('^s/', include('shorturls.urls')),
+	
+	#(r'^grappelli/', include('grappelli.urls')),
+	
+	
+	# Because I have the URL regexing words as sections, this pattern needs to be last.
+	# If you put anything after, the CONF will think it's a section and you won't get anywhere.
+	## THE FOLLOWING LINE MUST BE THE LAST URL IN THE CONF
 	(r'^(?P<section>[-\w]+)/', 'stories.views.index_section'),
+	## DO NOT ADD ANY URLS AFTER THIS LINE
 )
