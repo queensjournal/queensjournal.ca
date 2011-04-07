50fa1e24bd0a0f279c60f61f59fdfd28994fc256
tyler <tyler@tylerball.net>
2011-04-07 16:03:47 -0400

    trying to reduce some memory usage

--- galleries/models.py
+++ galleries/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import datetime
 from django.template.defaultfilters import slugify
 from stories.models import Story, Photo
 
 class Gallery(models.Model):
     name = models.CharField(max_length=255)
     story = models.ForeignKey(Story)
-    images = models.ManyToManyField(Photo)
+    images = models.ManyToManyField(Photo, limit_choices_to = {'creation_date__gt': datetime.datetime.now() - datetime.timedelta(weeks=8)})
 
     class Meta:
         verbose_name = "Photo Gallery"
