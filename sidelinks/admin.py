af7c687403008d8265f5ba1450d36f52ba6a0fcc
tyler <tyler@tylerball.net>
2011-04-12 09:40:22 -0400

    oops

--- sidelinks/admin.py
+++ sidelinks/admin.py
@@ -3,7 +3,6 @@ from sidelinks.models import SidebarLinkset, Link
 
 class SidelinkInline(admin.TabularInline):
 	model = Link
-	fields = ['text', 'link', 'css_class']
 
 class SidebarAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('name',)}
