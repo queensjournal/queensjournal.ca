136d6aa32ae6a8702e4cf42f40a1b5c7f845b682
tyler <tyler@tylerball.net>
2011-04-16 12:09:06 -0400

    oops

--- video/views.py
+++ video/views.py
@@ -14,7 +14,7 @@ def detail_video(request, datestring, slug):
 								context_instance=RequestContext(request))
 								
 def index_video(request):
-	video_list = get_list_or_404(Video, published=True)
+	video_list = get_list_or_404(Video, is_published=True)
 	latest_video = video_list[0]
 	other_videos = video_list[1:9]
 	return render_to_response('video/index_video.html',
