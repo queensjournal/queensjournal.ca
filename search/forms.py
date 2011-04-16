ba5f9d7bd15b7364937fa86414506bf54ef577ed
tyler <tyler@tylerball.net>
2011-04-16 00:27:36 -0400

    fancy search with tags

---
+++ search/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from haystack.forms import SearchForm, ModelSearchForm
+
+class SuperSearchForm(ModelSearchForm):
+	start_date = forms.DateField(required=False, widget=forms.HiddenInput)
+	end_date = forms.DateField(required=False, widget=forms.HiddenInput)
+	selected_facets = forms.CharField(required=False, widget=forms.HiddenInput)
+	
+	def search(self):
+		sqs = super(SuperSearchForm, self).search()
+		
+		if hasattr(self, 'cleaned_data') and self.cleaned_data['selected_facets']:
+			sqs = sqs.narrow(self.cleaned_data['selected_facets'])
+			
+		if self.cleaned_data['start_date']:
+			sqs = sqs.filter(pub_date__gte=self.cleaned_data['start_date'])
+			
+		if self.cleaned_data['end_date']:
+			sqs = sqs.filter(pub_date__lte=self.cleaned_data['end_date'])
+		
+		return sqs.models(*self.get_models())
--- search/urls.py
+++ search/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
 from haystack.views import FacetedSearchView
 from haystack.query import SearchQuerySet
-from haystack.forms import FacetedSearchForm
+from search.forms import SuperSearchForm
 from haystack.utils import Highlighter
 
 sqs = SearchQuerySet().facet('author').facet('tags').highlight()
 
 urlpatterns = patterns('haystack.views',
-    url(r'^$', FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs), name='haystack_search'),
+    url(r'^$', FacetedSearchView(form_class=SuperSearchForm, searchqueryset=sqs), name='haystack_search'),
 )
