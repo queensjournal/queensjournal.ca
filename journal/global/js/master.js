3f5c5a6deaa3e3b19465f167fc29d151a3546799
Tyler Ball <tyler@tylerball.net>
2011-06-09 17:57:45 -0400

    added blog menu

---
+++ journal/global/js/master.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    var queryhash = window.location.hash
+    if (queryhash == '#blogs') {
+        toggleBlogs();
+    }
+    $('.blog-link a span').append('<div id="blog-menu-arrow"></div>');
+    $('.blog-link a').click(toggleBlogs);
+    $('.blog-menu-hide a').click(hideBlogs);
+});
+function toggleBlogs() {
+    if ($('#menu-blogs').hasClass('active')) {
+        hideBlogs();
+    } else {
+        $('#menu-blogs').slideDown(500).addClass('active')
+        $('#menu-sections').delay(300).css('margin-bottom', '0.5em');
+        $('#blog-menu-arrow').show();
+    }
+}
+function hideBlogs() {
+    $('#menu-blogs').slideUp(300);
+    $('#menu-blogs').removeClass('active')
+    $('#menu-sections').css('margin-bottom', '1.5em');
+    $('#blog-menu-arrow').hide();
+}
