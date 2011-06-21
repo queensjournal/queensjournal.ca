16689ce4204c5fc92680932813e4bb8dd5620677
Tyler Ball <tyler@tylerball.net>
2011-06-21 03:18:38 -0400

    hot topics bar

---
+++ journal/custom/templatetags/tag_cloud.py
@@ -0,0 +1,32 @@
+import operator
+from django import template
+from tagging.models import Tag
+from tagging.utils import LOGARITHMIC, LINEAR
+
+from stories.models import Story
+from blog.models import Entry
+from video.models import Video
+
+register = template.Library()
+
+def do_cloud():
+    " returns a cloud "
+    story_list = [tag for tag in Tag.objects.cloud_for_model(Story, steps=3, distribution=LINEAR, filters=None, min_count=None)]
+    entry_list = [tag for tag in Tag.objects.cloud_for_model(Entry, steps=3, distribution=LINEAR, filters=None, min_count=None)]
+    video_list = [tag for tag in Tag.objects.cloud_for_model(Video, steps=2, distribution=LINEAR, filters=None, min_count=None)]
+    cloud = story_list + entry_list + video_list
+    cloud.sort(lambda a, b: cmp(a.name, b.name))
+    cloud1 = reduce(lambda l, x: x not in l and l.append(x) or l, cloud, [])
+    return {'cloud': cloud1}
+    
+def popular_tags():
+    " Top tags for a Thestar.com-style top topics bar. "
+    story_list = Tag.objects.usage_for_model(Story, counts=True)
+    entry_list = Tag.objects.usage_for_model(Entry, counts=True)
+    video_list = Tag.objects.usage_for_model(Video, counts=True)
+    tags = story_list + entry_list + video_list
+    tags.sort(key=operator.attrgetter('count'), reverse=True)
+    return {'tags': tags[:10]}
+
+register.inclusion_tag('tags/tag_cloud.html')(do_cloud)
+register.inclusion_tag('tags/popular_tags.html')(popular_tags)
