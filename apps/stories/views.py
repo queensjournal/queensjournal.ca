b14b9543472d9ef6f280c71e1be36ec2dbcd4ec1
Tyler Ball <tball@trapeze.com>
2011-08-07 20:11:51 -0400

    404 if tag not found

--- apps/stories/views.py
+++ apps/stories/views.py
@@ -94,7 +94,7 @@ def tags(request):
     
 def with_tag(request, tag, object_id=None, page=1): 
     unslug = tag.replace('+', ' ')
-    query_tag = Tag.objects.get(name=unslug)
+    query_tag = get_object_or_404(Tag, name=unslug)
     story_list = TaggedItem.objects.get_by_model(Story, query_tag).order_by('-pub_date')
     entry_list = TaggedItem.objects.get_by_model(Entry, query_tag).order_by('-pub_date')
     video_list = TaggedItem.objects.get_by_model(Video, query_tag).order_by('-pub_date')
