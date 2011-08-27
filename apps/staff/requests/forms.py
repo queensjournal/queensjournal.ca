a831c23298f8a63ccaabcfdb4c39017eb42c6c87
Tyler Ball <tball@trapeze.com>
2011-08-27 13:48:14 -0400

    Author links in PhotoRequests

--- apps/staff/requests/forms.py
+++ apps/staff/requests/forms.py
@@ -1,30 +1,7 @@
-##from django import newforms as forms
-##from django.contrib.auth.models import User
-##
-##attrs_dict = { 'class': 'required' }
-##
-##class LoginForm(forms.Form):
-##    username = forms.CharField(max_length=30,
-##                               widget=forms.TextInput(attrs=attrs_dict),
-##                               label=u'Username')
-##    password = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict),
-##                                label=u'Password')
-##
-##
-##class ChangePasswordForm(forms.Form):
-##    old_password = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict),
-##                                label=u'Old password')
-##    password1 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict),
-##                                label=u'New password')
-##    password2 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict),
-##                                label=u'Repeat new password (to catch typos)')
-##
-##    def clean_password2(self):
-##        """
-##        Validates that the two password inputs match.
-##        
-##        """
-##        if self.clean_data.get('password1', None) and self.clean_data.get('password2', None) and \
-##           self.clean_data['password1'] == self.clean_data['password2']:
-##            return self.clean_data['password2']
-##        raise forms.ValidationError(u'You must type the same password each time')
+from django.forms import ModelForm
+from staff.requests.models import PhotoRequest
+
+class RequestForm(ModelForm):
+    class Meta:
+        model = PhotoRequest
+        fields = ('subject', 'location', 'time', 'section', 'deadline', 'notes', 'status', )
