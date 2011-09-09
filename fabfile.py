e8137e6d6c23c19fc726a261c7215474ecc31cd3
Tyler Ball <tball@trapeze.com>
2011-09-09 17:58:59 -0400

    nice pulldump command

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,23 @@
 from fabric.api import *
 
-env.hosts=['3781lanru0j@queensjournal.ca']
-env.directory='/home/3781lanru0j/webapps/journal'
-env.activate='source /home/3781lanru0j/webapps/journal/bin/activate'
+env.hosts=['journal@queensjournal.ca']
+env.directory='webapps/journal'
+env.activate='source webapps/journal/bin/activate'
+supervisor_task = 'journal'
+
+local_django = '~/Sites/queensjournal.ca/apps'
+local_dumps = '~/Sites/queensjournal.ca/dumps'
+
+def local_env(command):
+    with cd(local_django):
+        local('source ../bin/activate && ' + command)
 
 def virtualenv(command):
     with cd(env.directory):
-        run(env.activate + '&&' + command)
+        run(env.activate + ' && ' + command)
+    
+def restart():
+    sudo('supervisorctl restart %s' % supervisor_task)
         
 def stage():
     local('git push origin master')
@@ -16,12 +27,23 @@ def stage():
     virtualenv('pip install -r requirements.txt')
     restart()
     
-def restart():
-    virtualenv('%s/apache2/bin/stop' % (env.directory))
-    virtualenv('%s/apache2/bin/start' % (env.directory))
-    
 def update():
     local('git push origin master')
     with cd(env.directory):
         run('git pull origin master')
     restart()
+
+def pulldump():
+    with cd(local_dumps) and settings(warn_only=True):
+        # Kill off any other dumps made today
+        try:
+            run('rm journal-`date +%F`*')
+            run('pg_dump -h localhost -U postgres --clean --no-owner --no-privileges journal > {}/dumps/journal-`date +%F`.sql'.format(env.directory))
+            with cd('%s/dumps' % env.directory):
+                run('tar -czf journal-`date +%F`.tar.gz journal-`date +%F`.sql')
+            local('scp {}:{}/dumps/journal-`date +%F`.tar.gz {}/'.format(env.host_string, env.directory, local_dumps))
+        except:
+            local('scp {}:{}/dumps/journal-`date +%F`.tar.gz {}/'.format(env.host_string, env.directory, local_dumps))
+        local('tar -xzvf journal-`date +%F`.tar.gz')
+    local_env('./manage.py dbshell < ../dumps/journal-`date +%F`.sql')
+
