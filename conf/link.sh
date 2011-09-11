cee74bdd44ed93bffbeb7e7579f4b8b431a4c0fc
Tyler Ball <tball@trapeze.com>
2011-09-11 12:04:46 -0400

    It's probably better that we cp, rather than ln

--- conf/link.sh
+++ conf/link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # link nginx
-sudo ln -s journal-nginx.conf /etc/nginx/conf.d/
+sudo cp -f journal-nginx.conf /etc/nginx/conf.d/
 
 # link gunicorn
-sudo ln -s journal-gunicorn.conf /etc/supervisor/conf.d/
+sudo cp -f journal-gunicorn.conf /etc/supervisor/conf.d/
 
-sudo ln -s journal-memcached.conf /etc/supervisor/conf.d/
+sudo cp -f journal-memcached.conf /etc/supervisor/conf.d/
