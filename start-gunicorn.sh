2e107b780e995c31aafd55310c1095b6066e1345
Tyler Ball <tball@trapeze.com>
2011-09-14 15:56:39 -0400

    path fix

--- start-gunicorn.sh
+++ start-gunicorn.sh
@@ -7,7 +7,7 @@ NUM_WORKERS=3
 # user/group to run as
 USER=journal
 cd /home/journal/webapps/journal/apps
-source ../bin/activate
+source bin/activate
 exec /home/journal/webapps/journal/bin/gunicorn_django -w $NUM_WORKERS \
     --user=$USER --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
