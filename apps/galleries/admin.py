1d3c999ddad35fc92569e7f617a243a05a805f84
Tyler Ball <tball@trapeze.com>
2011-10-09 12:21:50 -0400

    Removing PhotosPageOptions for now. Should use a better solution

--- apps/galleries/admin.py
+++ apps/galleries/admin.py
@@ -1,19 +1,7 @@
 import settings
 from django.contrib import admin
 from structure.models import Author
-from galleries.models import PhotosPageOptions, Gallery
-
-class PhotosPageAdmin(admin.ModelAdmin):
-    filter_horizontal = ('photographers',)
-    actions = None
-
-    #def has_add_permission(self, request):
-     #   return False
-        
-    def has_delete_permission(self, request, obj=None):
-        return False
-    
-admin.site.register(PhotosPageOptions, PhotosPageAdmin)
+from galleries.models import Gallery
 
 class GalleryAdmin(admin.ModelAdmin):
     filter_horizontal = ['images']
