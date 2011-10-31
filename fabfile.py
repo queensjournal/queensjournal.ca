625893d521e611544f9c1f5aadd42575d6a407f9
Tyler Ball <tball@trapeze.com>
2011-10-31 05:01:55 -0400

    updated fabfile with the knowledge

--- fabfile.py
+++ fabfile.py
@@ -1,36 +1,91 @@
+import os
+import fabric
+from random import Random
+from getpass import getpass
 from fabric.api import *
+from fabric.operations import prompt
+from fabric.contrib import django
+
+
+env.root_dir = os.path.abspath(os.path.dirname(__file__))
+env.unit = os.path.split(env.root_dir)[1]
+django.project('apps')
+from django.conf import settings
+
+env.path = "~/webapps/%(unit)s" % env
+env.source = "git@bitbucket.com:tylerball/%(unit)s.git" % env
+env.source_http = "https://tylerball@bitbucket.org/tylerball/%(unit)s.git" % env
+env.django_root = os.path.join(env.root_dir, 'apps/')
 
 env.hosts=['journal@queensjournal.ca']
-env.directory='webapps/journal'
 env.media='webapps/media'
 env.activate='source webapps/journal/bin/activate'
 supervisor_task = 'journal'
 
-local_django = '~/Sites/queensjournal.ca/apps'
-local_dumps = '~/Sites/queensjournal.ca/dumps'
+local_dumps = os.path.join(env.root_dir, 'dumps/')
 
-def local_env(command):
-    local('source %s/../bin/activate && %s' % (local_django, command))
+env.pip_requirements = "requirements/common.txt"
+env.pip_requirements_prod = "requirements/prod.txt"
+
+rng = Random()
+r_seed = "1234567890qwertyuiopasdfghjklzxcvbnm"
+env.secret_key = ""
+for i in range(30):
+    env.secret_key += rng.choice(r_seed)
+
+def echo(msg):
+    local('echo %s' % msg)
+
+def setup_localsettings():
+    with hide('running'):
+        env.password = getpass("Enter a password for your database")
+        with lcd(env.django_root):
+            echo("Creating settings_local.py")
+            local("sed 's/<projectname>/%s/g' settings_local.py.ex > settings_local.py" % env.unit)
+            local("sed 's/<password>/%s/g' settings_local.py > settings_local.py.1" % env.password)
+            local("sed 's/<secretkey>/%s/g' settings_local.py.1 > settings_local.py")
+            local("rm settings_local.py.1")
+
+def setup_localdb():
+    with hide('running'):
+        local("createuser --no-createrole --no-superuser --createdb --pwprompt %s" % env.unit)
+        local("createdb %(unit)s --owner %(unit)s" % env)
+        local("echo '*:*:%(unit)s:%(unit)s:%(password)s' >> ~/.pgpass" % env)
+        local("chmod 0600 ~/.pgpass")
+
+def setup_localreqs():
+    with hide('running'):
+        echo('updating requirements')
+        with lcd(env.root_dir):
+            local('pip install -r %s' % env.pip_requirements)
+            local('git submodule sync')
+            local('git submodule init')
+            local('git submodule update')
+
+def test_all():
+    with lcd(env.django_root):
+        local('./manage.py test')
+
+def setup_local():
+    setup_localreqs()
+    setup_localsettings()
+    setup_localdb()
+    test_all()
 
 def virtualenv(command):
-    with cd(env.directory):
+    with cd(env.path):
         run(env.activate + ' && ' + command)
-    
+
 def restart():
     sudo('supervisorctl restart %s' % supervisor_task)
-        
-def stage():
-    local('git push origin master')
-    local('pip freeze > requirements.txt')
-    with cd(env.directory):
-        run('git pull origin master')
-    virtualenv('pip install -r requirements.txt')
-    restart()
-    
+
 def update():
     local('git push origin master')
-    with cd(env.directory):
-        run('git pull origin master')
+    virtualenv('git pull origin master')
+    virtualenv('pip install -r %(pip_requirements)s' % env)
+    virtualenv('git submodule sync')
+    virtualenv('git submodule init')
+    virtualenv('git submodule update')
     restart()
 
 def pulldump():
@@ -39,15 +94,13 @@ def pulldump():
         try:
             run('rm journal-`date +%F`*')
             run('pg_dump -h localhost -U postgres --clean --no-owner --no-privileges journal > {}/dumps/journal-`date +%F`.sql'.format(env.directory))
-            with cd('%s/dumps' % env.directory):
+            with cd('%s/dumps' % env.path):
                 run('tar -czf journal-`date +%F`.tar.gz journal-`date +%F`.sql')
-            local('scp {}:{}/dumps/journal-`date +%F`.tar.gz {}/'.format(env.host_string, env.directory, local_dumps))
+            local('scp {}:{}/dumps/journal-`date +%F`.tar.gz {}/'.format(env.host_string, env.path, local_dumps))
         except:
-            local('scp {}:{}/dumps/journal-`date +%F`.tar.gz {}/'.format(env.host_string, env.directory, local_dumps))
+            local('scp {}:{}/dumps/journal-`date +%F`.tar.gz {}/'.format(env.host_string, env.path, local_dumps))
         local('tar -xzvf {}/journal-`date +%F`.tar.gz'.format(local_dumps))
     local_env('./manage.py dbshell < ../dumps/journal-`date +%F`.sql')
 
 def pullmedia():
-    with cd(env.media):
-        run('tar -czv --exclude="cache/" --exclude="photo_cache/" -f media-`date +%F`.tar.gz .')
-    local('scp {}:{}/media-`date +%F`.tar.gz {}/'.format(env.host_string, env.media, local_dumps))
+    local('rsync -auvz --progress --exclude="cache/" --exclude="photo_cache/" %(host_string)s:%(media)s/* ' % env + settings.MEDIA_ROOT)
