6b8715cf8f716fd570d8a337384e2f679c62d8ca
Tyler Ball <tball@trapeze.com>
2011-11-27 18:10:24 -0500

    fixed importerror

--- apps/sidebars/templatetags/sidebartags.py
+++ apps/sidebars/templatetags/sidebartags.py
@@ -1,54 +1,52 @@
-from datetime import datetime
 from django import template
-from stories.models import Story
 from structure.models import Section, Issue
-from sidebars.models import NewsCalendarItem, ArtsCalendarItem, TalkingHeadsItem, SportsCalendarItem
+from sidebars.models import PreviousStories
 from sidebars.settings import *
 
 register = template.Library()
 
 class SidebarItemsByIssueNode(template.Node):
-	"""
-	Returns sidebar items for a specific issue date.
-	section: a Section object, determines type of sidebar items.
-	num: Number of sidebar items to return.
-	issue: an Issue object, determines date range of sidebar items.
-	"""
-	def __init__(self, section, issue, num):
-		self.section = template.Variable(section)
-		self.num = float(num)
-		self.issue = template.Variable(issue)
+    """
+    Returns sidebar items for a specific issue date.
+    section: a Section object, determines type of sidebar items.
+    num: Number of sidebar items to return.
+    issue: an Issue object, determines date range of sidebar items.
+    """
+    def __init__(self, section, issue, num):
+        self.section = template.Variable(section)
+        self.num = float(num)
+        self.issue = template.Variable(issue)
 
-	def render(self, context):
-		# first, grab actual section/issue objects from context and
-		# grab SidebarItem class and template file for section
-		section_obj = self.section.resolve(context)
-		issue_obj = self.issue.resolve(context)
-		try:
-			sidebar_args = SIDEBAR_SECTIONS[section_obj.slug]
-			manager, template_file = sidebar_args[0], sidebar_args[1]
-		except:
-			# deals with situations where the sidebar slug doesn't exist in
-			# the settings dictionary. Defaults to "Previous stories."
-			manager, template_file = PreviousStories(Section.objects.filter(slug=section_obj.slug)[0], self.num), 'sidebars/previously_sidebar.html'
-		# create template nodelist
-		nodelist = template.loader.get_template(template_file).nodelist
-		# create context for nodelist
-		context.push()
-		if Issue.objects.published().latest() == issue_obj:
-			context['items'] = manager.get_future()
-		else:
-			try:
-				context['items'] = manager.get_by_issue(issue_obj)
-			except IndexError:
-				context['items'] = manager.get_future()
-		if len(list(context['items'])) == 0:
-			context['is_empty'] = True
-		# render sidebar template
-		output = nodelist.render(context)
-		context.pop()
-		return output
-		
+    def render(self, context):
+        # first, grab actual section/issue objects from context and
+        # grab SidebarItem class and template file for section
+        section_obj = self.section.resolve(context)
+        issue_obj = self.issue.resolve(context)
+        try:
+            sidebar_args = SIDEBAR_SECTIONS[section_obj.slug]
+            manager, template_file = sidebar_args[0], sidebar_args[1]
+        except:
+            # deals with situations where the sidebar slug doesn't exist in
+            # the settings dictionary. Defaults to "Previous stories."
+            manager, template_file = PreviousStories(Section.objects.filter(slug=section_obj.slug)[0], self.num), 'sidebars/previously_sidebar.html'
+        # create template nodelist
+        nodelist = template.loader.get_template(template_file).nodelist
+        # create context for nodelist
+        context.push()
+        if Issue.objects.published().latest() == issue_obj:
+            context['items'] = manager.get_future()
+        else:
+            try:
+                context['items'] = manager.get_by_issue(issue_obj)
+            except IndexError:
+                context['items'] = manager.get_future()
+        if len(list(context['items'])) == 0:
+            context['is_empty'] = True
+        # render sidebar template
+        output = nodelist.render(context)
+        context.pop()
+        return output
+
 def do_section_sidebar(parser, token):
     """
     {% sidebar section_obj issue_obj num %}
