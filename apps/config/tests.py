6c4a5a9224f8e46f5930bf86040de046804dbd1b
Tyler Ball <tball@trapeze.com>
2012-05-31 23:05:58 -0400

    Upgraded to Django 1.4, consolidated requirements.txt

--- apps/config/tests.py
+++ apps/config/tests.py
@@ -1,16 +1,9 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+import factory
+from config.models import SiteConfig
+from structure.tests import SectionFactory
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SiteConfigFactory(factory.Factory):
+    FACTORY_FOR = SiteConfig
+
+    featured_tags = 'test, test2, test3'
