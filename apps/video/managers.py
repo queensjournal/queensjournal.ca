f084989b64a31854835f1443c79d5fdfc3a268d1
Tyler Ball <tball@trapeze.com>
2012-06-04 18:46:42 -0400

    Added a manager to Video to deal with published status.

---
+++ apps/video/managers.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class PublishedVideoManager(models.Manager):
+    def get_query_set(self):
+        return super(PublishedVideoManager, self).get_query_set().filter(
+            is_published=True, pub_date__lt=datetime.datetime.now())
