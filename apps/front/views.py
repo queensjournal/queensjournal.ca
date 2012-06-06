4e6e96d3667b1b57929dcd74ffe4037250de5766
Tyler Ball <tball@trapeze.com>
2012-06-05 21:30:18 -0400

    better view subclassing for dealing with Issue context

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,37 +1,47 @@
-import datetime
 from itertools import chain
 from operator import attrgetter
 from django.views.generic.base import TemplateView
-from structure.models import Issue, FrontPageConfig, FrontConfig, SectionFrontConfig
+from structure.models import Issue
 from stories.models import Story
 from blog.models import Entry
 from video.models import Video
+from config.models import SiteConfig
 
 
-class Front(TemplateView):
+class CurrentView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['sections'] = Issue.objects.latest().sections \
+            .flatplansection_set.all()
+        return context
+
+
+class FrontView(CurrentView):
     '''
     Front Page
     '''
     template_name = 'front.html'
 
     def get_context_data(self, **kwargs):
-        self.context = {}
-        latest_stories = Story.objects.filter(status='p', \
-            pub_date__lt=datetime.datetime.now()).order_by('-pub_date')[:5]
-        latest_entries = Entry.objects.filter(is_published=True).order_by('-pub_date')[:5]
+        context = super(FrontView, self).get_context_data(**kwargs)
+
+        latest_stories = Story.published.order_by('-pub_date')[:10]
+        latest_entries = Entry.objects.filter(is_published=True) \
+            .order_by('-pub_date')[:10]
         latest_video = Video.published.latest('pub_date')
-        self.context['latest_stories'] = sorted(chain(latest_stories, latest_entries), \
-            key=attrgetter('pub_date'))[:5]
+        context['latest_stories'] = sorted(chain( \
+            latest_stories, latest_entries), key=attrgetter('pub_date'))[:5]
 
         latest_section = []
-        #for section in self.context['config'].sections.flatplansection_set.all():
-            #latest_section.extend(Story.objects.filter(section=section.section,
-                #featured=True, storyphoto__isnull=False, \
-                #pub_date__lt=datetime.datetime.now()).order_by('-pub_date')[:1])
+        for section in context['sections']:
+            latest_section.extend(Story.published.filter(
+                section=section.section, featured=True, \
+                storyphoto__isnull=False).order_by('-pub_date')[:1])
 
-        #self.context['featured'] = self.context['config'].featuredstory_set.all().\
-            #order_by('story_order')
-        self.context['latest_entries'] = latest_entries
-        self.context['latest_section'] = latest_section
-        self.context['latest_video'] = latest_video
-        return self.context
+        config = SiteConfig.get()
+        context['featured'] = config.featuredstory_set.all() \
+            .order_by('story_order')
+        context['latest_entries'] = latest_entries
+        context['latest_section'] = latest_section
+        context['latest_video'] = latest_video
+        return context
