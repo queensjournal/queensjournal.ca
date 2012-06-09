29a8a5a4c26ecf4f91f42666f8fd75faa755b969
Tyler Ball <tball@trapeze.com>
2012-06-09 14:22:52 -0400

    Added permissions for config model

--- apps/config/admin.py
+++ apps/config/admin.py
@@ -13,15 +13,24 @@ class FeaturedInlineForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(FeaturedInlineForm, self).__init__(*args, **kwargs)
         start_date = datetime.datetime.now() - datetime.timedelta(150)
-        self.fields['story'].queryset = Story.objects.filter(pub_date__gte=start_date).order_by('-pub_date')
+        self.fields['story'].queryset = Story.objects.filter(
+            pub_date__gte=start_date).order_by('-pub_date')
+
 
 class FeaturedInline(admin.TabularInline):
     form = FeaturedInlineForm
     model = FeaturedStory
 
+
 class ConfigAdmin(admin.ModelAdmin):
     inlines = [
         FeaturedInline,
     ]
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 admin.site.register(SiteConfig, ConfigAdmin)
