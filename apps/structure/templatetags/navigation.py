c6ae45977c9dc34af27b4f27858f6e36cfabc00e
Tyler Ball <tball@trapeze.com>
2012-06-10 21:04:00 -0400

    Moving navigation templatetags, cleanup

--- apps/structure/templatetags/navigation.py
+++ apps/structure/templatetags/navigation.py
@@ -1,35 +1,33 @@
 from django import template
-from structure.models import Issue, FrontConfig
+from structure.models import Issue
 from stories.models import Story
 from blog.models import Blog
 
 
 register = template.Library()
 
+
+def get_sections(context):
+    issue = context.get('issue')
+    if issue:
+        sections = issue.sections.get_sections()
+    else:
+        sections = Issue.objects.latest().sections.get_sections()
+    return sections
+
+
 def menu_sections(context):
     """
     Returns the HTML code for the sections menu of a given issue.
     """
     params = {}
-    back_issue = context.get('back_issue')
-    if back_issue is True:
-        params['back_issue'] = back_issue
-        try:
-            curr_issue = FrontConfig.objects.get(issue=context.get('issue'))
-        except FrontConfig.DoesNotExist:
-            curr_issue = context.get('issue')
-    else:
-        try:
-            curr_issue = FrontConfig.objects.latest('pub_date')
-        except FrontConfig.DoesNotExist:
-            return None
-    if context.get('story_set'):
-        params['show_section_link'] = False
-    else:
-        params['show_section_link'] = True
-    params['sections'] = curr_issue.sections
+    params['sections'] = get_sections(context)
     params['config'] = context.get('config')
     return params
+
+
+register.inclusion_tag('global/menu_sections.html', takes_context=True)(menu_sections)
+
 
 class OtherStoriesListNode(template.Node):
     """
@@ -53,6 +51,7 @@ class OtherStoriesListNode(template.Node):
             context['no_other_stories'] = True
         return ''
 
+
 def do_other_stories(parser, token):
     """
     {% other_stories section_obj issue_obj num %}
@@ -65,6 +64,10 @@ def do_other_stories(parser, token):
     else:
         return OtherStoriesListNode(bits[1], bits[2])
 
+
+register.tag('other_stories', do_other_stories)
+
+
 def menu_blogs(context):
     '''
     Provides HTML code for active blogs
@@ -73,29 +76,18 @@ def menu_blogs(context):
     params['blogs'] = Blog.objects.filter(active=True)
     return params
 
+
+register.inclusion_tag('global/menu_blogs.html', takes_context=True)(menu_blogs)
+
+
 def menu_mobile(context):
     """
     Returns the HTML code for the sections menu of a given issue.
     """
     params = {}
-    back_issue = context.get('back_issue')
-    if back_issue is True:
-        params['back_issue'] = back_issue
-        try:
-            curr_issue = FrontConfig.objects.get(issue=context.get('issue'))
-        except FrontConfig.DoesNotExist:
-            curr_issue = context.get('issue')
-    else:
-        curr_issue = FrontConfig.objects.latest('pub_date')
-    if context.get('story_set'):
-        params['show_section_link'] = False
-    else:
-        params['show_section_link'] = True
-    params['sections'] = curr_issue.sections
+    params['sections'] = get_sections(context)
     params['config'] = context.get('config')
     return params
 
-register.inclusion_tag('global/menu_sections.html', takes_context=True)(menu_sections)
-register.tag('other_stories', do_other_stories)
-register.inclusion_tag('global/menu_blogs.html', takes_context=True)(menu_blogs)
+
 register.inclusion_tag('global/menu.html', takes_context=True)(menu_mobile)
