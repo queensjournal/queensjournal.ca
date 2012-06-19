31ac1de49353cdf2298cc4f4d05a1a5a221453b4
Tyler Ball <tball@trapeze.com>
2012-06-18 20:56:44 -0400

    sequential story factory.

--- apps/stories/factories.py
+++ apps/stories/factories.py
@@ -1,6 +1,6 @@
-import datetime
+from datetime import datetime, timedelta
 import factory
-from structure.factories import SectionFactory, AuthorFactory
+from structure.factories import SectionFactory
 from stories.models import Story
 
 
@@ -10,5 +10,7 @@ class StoryFactory(factory.Factory):
     head = 'Queen\'s Journal Implements testing framework, finally.'
     deck = u'Editor-in-Chief says this will help detect issues earlier'
     slug = u'journal-implements-tests'
-    pub_date = datetime.datetime.now()
+    pub_date = factory.Sequence(lambda n: datetime.now() - \
+        timedelta(weeks=(n)), type=int)
     section = factory.SubFactory(SectionFactory)
+    status = 'p'
