ddee57f7d7c7f6263ff08a24dc7272578c3c2c9a
Tyler Ball <tball@trapeze.com>
2012-06-27 23:51:47 -0400

    updated gunicorn script

--- start-gunicorn.sh
+++ start-gunicorn.sh
@@ -2,12 +2,11 @@
 
 # Gunicorn start script
 set -e
-LOGFILE=/home/journal/logs/journal-guinicorn.log
+LOGFILE=/home/journal/logs/journal-gunicorn.log
 NUM_WORKERS=3
 # user/group to run as
 USER=journal
-cd /home/journal/webapps/journal/apps
-source ~/webapps/journal/bin/activate
-exec /home/journal/webapps/journal/bin/gunicorn_django -w $NUM_WORKERS \
+source /home/journal/journal/bin/activate
+exec /home/journal/journal/bin/gunicorn_django -w $NUM_WORKERS \
     --user=$USER --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
