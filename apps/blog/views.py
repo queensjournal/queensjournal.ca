7e80d3403a44f8e7402d0e1d7ede15e33242eb4e
Tyler Ball <tball@trapeze.com>
2012-07-01 18:15:48 -0400

    blog tests and pep8

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,26 +1,31 @@
 from datetime import datetime
 from django.views.generic.list_detail import object_detail, object_list
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponseRedirect
 from blog.models import Blog, Entry
 from structure.models import Author
 from tagging.models import Tag
 from tagging.utils import calculate_cloud
 
+
 def all_blogs(request, active=True):
     """
     List of all blogs. Defaults to listing active blogs.
     """
-    qs = Blog.objects.filter(active=active).order_by('order')
+    blogs = Blog.objects.filter(active=active).order_by('order')
     featured = []
-    for blog in qs:
+    for blog in blogs:
         featured.extend(Entry.objects.has_photos(blog)[:1])
     latest = Entry.objects.filter(pub_date__lte=datetime.now()).order_by('-pub_date')[:5]
-    c = {'active': active,
-         'archives': Blog.objects.filter(active=False).count() > 0,
+    c = {
+        'blogs': blogs,
+        'active': active,
+        #'archives': Blog.objects.filter(active=False).count() > 0,
         'featured': featured,
-        'latest': latest,}
-    return object_list(request, queryset=qs, extra_context=c)
+        'latest': latest,
+    }
+    return render(request, 'blog/blog_list.html', c)
+
 
 def blog_index(request, blog, page=1):
     """
@@ -37,6 +42,7 @@ def blog_index(request, blog, page=1):
     return object_list(request, queryset=qs, paginate_by=10, allow_empty=False,
         page=page, extra_context=c)
 
+
 def blog_archive_month(request, blog, year, month, page=1):
     """
     Monthly archives. Displays all blog posts in a given month. Paginated.
@@ -45,20 +51,23 @@ def blog_archive_month(request, blog, year, month, page=1):
     month = int(month)
     qs = Entry.objects.get_month_on_blog(blog, year, month)
     if qs.count() > 0:
-        c = {'blog': get_object_or_404(Blog, slug=blog),
-             'month': datetime(year,month,1)}
+        c = {
+            'blog': get_object_or_404(Blog, slug=blog),
+            'month': datetime(year, month, 1)
+        }
         if month == 1:
-            c['prev_month'] = datetime(year-1,12,1)
+            c['prev_month'] = datetime(year - 1, 12, 1)
         else:
-            c['prev_month'] = datetime(year,month-1,1)
+            c['prev_month'] = datetime(year, month - 1, 1)
         if month == 12:
-            c['next_month'] = datetime(year+1,1,1)
+            c['next_month'] = datetime(year + 1, 1, 1)
         else:
-            c['next_month'] = datetime(year,month+1,1)
+            c['next_month'] = datetime(year, month + 1, 1)
         return object_list(request, queryset=qs, paginate_by=10, allow_empty=False,
             extra_context=c, page=page)
     else:
         raise Http404
+
 
 def blog_detail(request, blog, year, month, slug):
     """
@@ -66,9 +75,12 @@ def blog_detail(request, blog, year, month, slug):
     """
     year = int(year)
     month = int(month)
-    c = {'blog': get_object_or_404(Blog, slug=blog)}
-    qs = Entry.objects.get_month_on_blog(blog, year,month)
-    return object_detail(request, queryset=qs, slug_field='slug', slug=slug, extra_context=c)
+    c = {
+        'blog': get_object_or_404(Blog, slug=blog),
+        'entry': Entry.objects.get_month_on_blog(blog, year, month),
+    }
+    return render(request, 'blog/entry_detail.html', c)
+
 
 def blog_draft_detail(request, post_id):
     """
@@ -93,6 +105,7 @@ def blog_draft_detail(request, post_id):
         else:
             raise Http404
 
+
 def blog_all_authors(request, blog):
     """
     All author profiles for an individual blog on one page. Whee!
@@ -100,4 +113,3 @@ def blog_all_authors(request, blog):
     qs = Author.objects.select_related(depth=2).filter(entry__blog__slug__exact=blog)
     c = {'blog': get_object_or_404(Blog, slug=blog)}
     return object_list(request, queryset=qs, extra_context=c)
-
