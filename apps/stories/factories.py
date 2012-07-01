e8a4f97f72bec0b63675f4ced6f97f8254b48a0b
Tyler Ball <tball@trapeze.com>
2012-07-01 18:36:37 -0400

    fixed some failing tests

--- apps/stories/factories.py
+++ apps/stories/factories.py
@@ -7,7 +7,7 @@ from stories.models import Story
 class StoryFactory(factory.Factory):
     FACTORY_FOR = Story
 
-    head = 'Queen\'s Journal Implements testing framework, finally.'
+    head = 'testing'
     deck = u'Editor-in-Chief says this will help detect issues earlier'
     slug = u'journal-implements-tests'
     issue = factory.SubFactory(IssueFactory)
--- apps/video/tests.py
+++ apps/video/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
-from utils.tests import SiteTestHelper
+from utils import SiteTestHelper
 from video.factories import VideoFactory
 
 
