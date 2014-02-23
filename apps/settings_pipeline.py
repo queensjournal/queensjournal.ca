24301f9448e914c7092dbe03dd39f31b98b77089
Tyler Ball <tyler@tylerball.net>
2014-02-23 10:38:40 -0500

    add bootstrap carousel, cleanup old JS

--- apps/settings_pipeline.py
+++ apps/settings_pipeline.py
@@ -3,6 +3,7 @@ PIPELINE_CSS = {
         'source_filenames': (
             'css/layout.scss',
             'css/modules.scss',
+            'css/bootstrap-carousel.css',
         ),
         'output_filename': 'compiled/css/core-styles.grouped.css',
         'extra_context': {
@@ -21,19 +22,9 @@ PIPELINE_CSS = {
 PIPELINE_JS = {
     'core-scripts': {
         'source_filenames': (
-            'js/jquery-1.6.1.min.js',
-            'js/master.js',
+            'js/jquery-1.11.0.js',
+            'js/carousel.js',
         ),
         'output_filename': 'compiled/js/core-scripts.min.js',
-    },
-
-    'mobile-scripts': {
-        'source_filenames': (
-            'js/jquery.masonry.js',
-            'js/jquery.anythingslider.min.js',
-            'js/jquery.swipe.js',
-            'js/mobile.js',
-        ),
-        'output_filename': 'compiled/js/mobile-scripts.min.js',
     },
 }
