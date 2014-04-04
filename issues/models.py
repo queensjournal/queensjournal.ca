d78d538b1acd14c225758ed433c1ac55473d5d3c
Tyler Ball <tyler@tylerball.net>
2014-04-03 20:58:25 -0400

    archive ordering and styling

--- issues/models.py
+++ issues/models.py
@@ -6,6 +6,9 @@ class Volume(models.Model):
     volume = models.PositiveSmallIntegerField(unique=True)
     issuu_embed = models.TextField(null=True, blank=True)
 
+    class Meta:
+        ordering = ['-volume']
+
     def get_years(self):
         """
         Returns a DateQuerySet containing the year span for all issues in the
@@ -13,8 +16,17 @@ class Volume(models.Model):
         """
         return self.issue_set.dates('pub_date', 'year')
 
+    def issue_list(self):
+        return self.issue_set.order_by('issue')
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('archive-volume-detail', (), {
+                'volume': self.volume,
+                })
+
     def __unicode__(self):
-        return '%i' % (self.volume,)
+        return 'Volume %i' % (self.volume,)
 
 
 class Issue(models.Model):
@@ -39,11 +51,19 @@ class Issue(models.Model):
         ordering = ['-pub_date']
         get_latest_by = 'pub_date'
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ('issue-detail', (), {
+            'volume': self.volume.volume,
+            'issue': self.issue,
+        })
+
     def __unicode__(self):
         if self.extra != "":
-            return 'Vol. %s, %s (special issue)' % (self.volume, self.extra)
+            return 'Vol. %s, %s (special issue)' % (self.volume.volume,
+                self.extra)
         else:
-            return 'Vol. %s, Issue %i' % (self.volume, self.issue)
+            return 'Vol. %s, Issue %i' % (self.volume.volume, self.issue)
 
     def get_featured_stories(self):
         stories = []
