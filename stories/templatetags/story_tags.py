c2414c2b47b8819bc67cbebfeb94c5076da4f97f
Tyler Ball <tyler@tylerball.net>
2014-04-03 21:05:15 -0400

    add "last updated" to site header and refactor "latest issue"

--- stories/templatetags/story_tags.py
+++ stories/templatetags/story_tags.py
@@ -5,8 +5,17 @@ register = template.Library()
 
 
 @register.assignment_tag
-def get_latest_stories(section=None, *args, **kwargs):
+def get_latest_stories(section=None, limit=10, *args, **kwargs):
     if section:
-        return section.get_latest_stories()
+        return section.get_latest_stories()[:limit]
     else:
-        return Story.objects.latest()
+        return Story.objects.latest()[:limit]
+
+
+@register.assignment_tag
+def get_last_updated():
+    try:
+        latest = Story.objects.latest().pub_date
+    except Story.DoesNotExist:
+        return None
+    return latest
